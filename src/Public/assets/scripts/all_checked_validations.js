af44e36cb93607abd3c265f907152f653c23c1aa
zestard <test.zestard@gmail.com>
2016-01-25 13:45:57 +0530

    file updated

--- src/Public/assets/scripts/all_checked_validations.js
+++ src/Public/assets/scripts/all_checked_validations.js
@@ -2,7 +2,7 @@ $(document).ready(function()
 {
     $('.all_checked').click(function ()
     {
-        if($(this).attr("checked") === "checked")
+        if($(this).is(':checked'))
         {
             $('.person_id_checkbox').attr("checked","checked");
             $('.person_id_checkbox').prop("checked","checked");
@@ -14,7 +14,7 @@ $(document).ready(function()
             $('.person_id_checkbox').parent('span').removeClass("checked");
         }
 
-        if($(this).attr("checked") === "checked")
+        if($(this).is(':checked'))
         {
             $('.company_id_checkbox').attr("checked","checked");
             $('.company_id_checkbox').prop("checked","checked");
@@ -26,7 +26,7 @@ $(document).ready(function()
             $('.company_id_checkbox').parent('span').removeClass("checked");
         }
 
-        if($(this).attr("checked") === "checked")
+        if($(this).is(':checked'))
         {
             $('.vacancy_id_checkbox').attr("checked","checked");
             $('.vacancy_id_checkbox').prop("checked","checked");
@@ -38,7 +38,7 @@ $(document).ready(function()
             $('.vacancy_id_checkbox').parent('span').removeClass("checked");
         }
 
-        if($(this).attr("checked") === "checked")
+        if($(this).is(':checked'))
         {
             $('.user_id_checkbox').attr("checked","checked");
             $('.user_id_checkbox').prop("checked","checked");
@@ -48,6 +48,18 @@ $(document).ready(function()
         {
             $('.user_id_checkbox').removeAttr("checked");            
             $('.user_id_checkbox').parent('span').removeClass("checked");
+        }
+
+        if($(this).is(':checked'))
+        {
+            $('.role_id_checkbox').attr("checked","checked");
+            $('.role_id_checkbox').prop("checked","checked");
+            $('.role_id_checkbox').parent('span').addClass("checked");
+        }
+        else
+        {
+            $('.role_id_checkbox').removeAttr("checked");            
+            $('.role_id_checkbox').parent('span').removeClass("checked");
         }
         
     });
